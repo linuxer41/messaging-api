7fef2becd5d77f980771ef7037c4478c84298344
Hitesh Dutt <er.hiteshdutt@gmail.com>
2024-08-26 14:55:07 +0530

    removing lint error from github

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,7 @@ app.use(express.json());
 app.use("/", routes);
 app.all("*", (_: Request, res: Response) => res.status(404).json({ error: "URL not found" }));
 
-const host = process.env.HOST || "0.0.0.0";
-const port = Number(process.env.PORT || 3000);
+const port = Number(process.env.PORT ?? 3000);
 
 (async () => {
 	await init();
